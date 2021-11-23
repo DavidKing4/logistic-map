a5679b311bbc0655ac239de432e9735fea8889dc
David King <davidking41998@gmail.com>
2021-11-23 23:50:10 +0100

    results output to png

--- LogisticMap.py
+++ LogisticMap.py
@@ -1,5 +1,7 @@
 from math import floor
 import numpy as np
+import struct
+import zlib
 
 
 def lmap(c: float, x: float) -> float:  # logistic map
@@ -29,25 +31,56 @@ def join_array(array):
     return "".join(["O" if x else "-" for x in list(array)])
 
 
-def ascii_display(pixels):
+def ascii_display(pixels) -> None:
     for line in pixels:
         print(join_array(line))
 
 
-def main():
-    res = "400, 230"  # input("resolution(width, height):")
+def pixels_to_png(pixels, w: int, h: int) -> None:
+    png = b"\x89PNG\r\n\x1A\n"
+    png += struct.pack(">I", 13)
+    IHDR = b"IHDR"
+    IHDR += struct.pack(">I", w - 1)
+    IHDR += struct.pack(">I", h - 1)
+    IHDR += struct.pack(">B", 8)  # bit_depth
+    IHDR += struct.pack(">B", 0)  # color_type
+    IHDR += struct.pack(">B", 0)  # compression_method
+    IHDR += struct.pack(">B", 0)  # filter_method
+    IHDR += struct.pack(">B", 0)  # interlace_method
+    png += IHDR
+    png += struct.pack(">I", zlib.crc32(IHDR))
+    raw = b""
+    for y in range(h - 1, 0, -1):
+        for x in range(w):
+            p = 255 if pixels[x][y] else 0
+            raw += struct.pack(">B", p)
+    compressor = zlib.compressobj()
+    compressed = compressor.compress(raw)
+    compressed += compressor.flush()
+    png += struct.pack(">I", len(compressed))
+    png += b"IDAT" + compressed
+    png += struct.pack(">I", zlib.crc32(b"IDAT" + compressed))
+    png += struct.pack(">I", 0)
+    png += b"IEND" + struct.pack(">I", zlib.crc32(b"IEND"))
+    with open("logistic.png", "wb") as image:
+        image.write(png)
+
+
+def main() -> None:
+    res = input("resolution(width, height):")
+    itterations = input("desired number of itterations:")
     w, h = [int(x) for x in res.split(",")]
     y_vals = np.array([(y + 1) / (h + 1) for y in range(h)])
     c_vals = np.array([x / w for x in range(w)]) * 3 + 1
     results = np.zeros((w, h))
     for i, c in enumerate(c_vals):
-        results[i] = log_map_rep(c, y_vals, 500)
+        results[i] = log_map_rep(c, y_vals, 1000)
     pixels = np.zeros((w, h), dtype=np.int8)
     for x, column in enumerate(results):
         for y in y_val_to_index(column, h):
             pixels[x][int(y)] = 1
-    return pixels
+    pixels_to_png(pixels, w, h)
 
 
 if __name__ == "__main__":
-    pixels = main()
+    main()
